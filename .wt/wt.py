7fb991326f26fceddb1d48c698ed22eaa73c2c41
alx <alx@mini.local>
2014-03-15 02:03:41 +0100

    [wt.py] Launch virtualenv interpreter instead of bash.

--- .wt/wt.py
+++ .wt/wt.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
+"""
+Usage:
+  {program_name}
+"""
 import git
 import worktry as wt
 
 def init_env():
     """
     """
-    import os, sys, subprocess
+    import os
 
-    shell_cmd = os.environ.get('SHELL', '/bin/sh')
     wt_dir = os.path.abspath(os.path.dirname(__file__))
     if wt_dir not in os.environ.get('PYTHONPATH', ''):
         os.environ['PYTHONPATH'] = "{}{}{}/.wt" .format(os.environ.get('PYTHONPATH', ''),
@@ -16,12 +19,21 @@ def init_env():
                                                         else "",
                                                         wt_dir)
 
-    subprocess.call(shell_cmd, env=os.environ, shell=True)
+
+def call_python():
+    import os, subprocess, sys
+
+    subprocess.call([sys.executable, '-i', '-c' 
+                     "import os, sys;"
+                     "execfile(os.environ['PYTHONSTARTUP']) if os.environ.get('PYTHONSTARTUP') else None;"
+                     "import worktry as wt"],
+                    env=os.environ, shell=True)
 
 def materialize(project_name, settings):
     """
     """
     print project_name, settings
+    init_env()
 
 if __name__ == "__main__":
     import json, os, sys
@@ -36,4 +48,5 @@ if __name__ == "__main__":
         materialize(p, projects[p])
 
     init_env()
+    call_python()
     print "END"
